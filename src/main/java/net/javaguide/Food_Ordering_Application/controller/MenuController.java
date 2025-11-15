6afd910c0066eace82f92fb807056461962ccdfc
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2025-11-16 02:50:25 +0530

    [] - change for testing

--- src/main/java/net/javaguide/Food_Ordering_Application/controller/MenuController.java
+++ src/main/java/net/javaguide/Food_Ordering_Application/controller/MenuController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/restaurant")
+@RequestMapping("/api/restaurants")
 public class MenuController {
 
     private final MenuService menuService;
