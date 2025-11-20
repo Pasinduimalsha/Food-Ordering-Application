0a49396f6b0eda5becc74d9e994ac3c0b8755bf6
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2025-11-20 23:23:17 +0530

    updated java version

--- mvn-script.sh
+++ mvn-script.sh
@@ -1,3 +1,10 @@
 sudo apt update
+sudo apt install openjdk-17-jdk -y
+
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-17-openjdk-amd64/bin/java 1
+sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-17-openjdk-amd64/bin/javac 1
+sudo update-alternatives --config java
+sudo update-alternatives --config javac
+
 sudo apt install maven -y
 mvn -version
